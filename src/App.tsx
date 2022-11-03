8cd98cb51e1f4dc829dd0b28d51789db70f8c4ad
pankajkoirala <pankaj.koirala.77@gmail.com>
2022-11-03 19:48:25 +0545

    fix: first commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Chakra_calender from './Chakra_calender';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{    // eslint-disable-next-line react/jsx-pascal-case
+}    <Chakra_calender values={{}} colors={['#adad85', '#004d1a', '#009933', '#00e64d', '#4dff88']} cellSpecing={2} />
     </div>
   );
 }
