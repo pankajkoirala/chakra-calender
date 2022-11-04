de9e532dcdfa4f86e1bccb58c1bc50e2ff05b5f0
pankajkoirala <pankaj.koirala.77@gmail.com>
2022-11-04 13:17:27 +0545

    feat: custom tooltip added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-
-import React from 'react';
-import './App.css';
-import Chakra_calender from './../src/Chakra_calender/index';
+/* eslint-disable react/jsx-pascal-case */
+import React from "react";
+import "./App.css";
+import Chakra_calender from "./../src/Chakra_calender/index";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-{    // eslint-disable-next-line react/jsx-pascal-case
-}    <Chakra_calender values={{'2022-4-6':2,'2022-5-6':3,'2022-7-6':4}} colors={['#adad85', '#004d1a', '#009933', '#00e64d', '#4dff88']} cellSpecing={2}  legend={['legend','legend-1','legend-2','legend-3']} />
+      <Chakra_calender
+        toltip={(date, message) => (
+          <Box padding={"0px"} margin={"0px"} borderRadius="4px">
+            <Box>TodayDate:{date}</Box>
+            <Box>message:{message}</Box>
+          </Box>
+        )}
+        values={{
+          "2022-5-6": { value: 1, message: "sick" },
+          "2022-5-5": { value: 1, message: "sick" },
+          "2022-5-3": { value: 1, message: "sick" },
+        }}
+        colors={["#adad85", "#004d1a", "#009933", "#00e64d", "#4dff88"]}
+        cellSpecing={2}
+        legend={["legend", "legend-1", "legend-2", "legend-3"]}
+      />
     </div>
   );
 }
--- src/Chakra_calender/Chakra-calender.tsx
+++ src/Chakra_calender/Chakra-calender.tsx
@@ -1,66 +1,68 @@
-import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
-import { Tooltip } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Tooltip } from "@chakra-ui/react";
+import React from "react";
 
 interface Iprops {
-  values: { [key: string]: number }
-  colors: string[]
-  cellHeight?: string
-  cellWidth?: string
-  hasTooltip?: boolean
-  cellSpecing?: number
-  gridGap?: string
-  padding?: string
-  backgroundColor?: string
-  monthsLabel?: boolean
-  monthsLabelFontSize?: string
-  daysLabel?: boolean
-  daysLabelFontSize?: string
-  cellBorderRadius?: string
-  legendGap?: string
-  legend?: string[]
-  legendLabelGap?: string
-  legendLabelBorderRadius?: string
-  legendLabelFontSize?: string
+  values: { [key: string]: { value: number; message: string } };
+  colors: string[];
+  cellHeight?: string;
+  cellWidth?: string;
+  hasTooltip?: boolean;
+  cellSpecing?: number;
+  gridGap?: string;
+  padding?: string;
+  backgroundColor?: string;
+  monthsLabel?: boolean;
+  monthsLabelFontSize?: string;
+  daysLabel?: boolean;
+  daysLabelFontSize?: string;
+  cellBorderRadius?: string;
+  legendGap?: string;
+  legend?: string[];
+  legendLabelGap?: string;
+  legendLabelBorderRadius?: string;
+  legendLabelFontSize?: string;
+  toltip?: (date: string, message: string) => React.ReactNode;
 }
 const ChakraCalender = ({
   values,
-  colors = ['#adad85', '#004d1a', '#009933', '#00e64d', '#4dff88'],
-  cellHeight = '12px',
-  cellWidth = '12px',
+  colors = ["#adad85", "#004d1a", "#009933", "#00e64d", "#4dff88"],
+  cellHeight = "12px",
+  cellWidth = "12px",
   hasTooltip = true,
   cellSpecing = 1,
-  gridGap = '12px',
-  padding = '10px',
-  backgroundColor = '#f5f5f0',
+  gridGap = "12px",
+  padding = "10px",
+  backgroundColor = "#f5f5f0",
   monthsLabel = true,
   daysLabel = true,
-  monthsLabelFontSize = '10px',
-  daysLabelFontSize = '10px',
-  cellBorderRadius = '4px',
-  legendGap = '12px',
+  monthsLabelFontSize = "10px",
+  daysLabelFontSize = "10px",
+  cellBorderRadius = "4px",
+  legendGap = "12px",
   legend = [],
-  legendLabelGap = '14px',
-  legendLabelBorderRadius = '8px',
-  legendLabelFontSize = '12px',
+  legendLabelGap = "14px",
+  legendLabelBorderRadius = "8px",
+  legendLabelFontSize = "12px",
+  toltip,
   ...others
 }: Iprops) => {
-  const getDays = (year: any, month: any) =>
-    new Date(year, month + 1, 0).getDate()
+  const getDays = (year: number, month: number) =>
+    new Date(year, month + 1, 0).getDate();
 
   function getMonthNames(
     date = new Date(new Date().setMonth(new Date().getMonth() - 11))
   ) {
-    let d = new Date(+date)
-    d.setDate(1)
-    let result = []
+    let d = new Date(+date);
+    d.setDate(1);
+    let result = [];
     for (let i = 0; i < 12; i++) {
       result.push({
         year: d.getFullYear(),
         mothInNumber: d.getMonth(),
-        mothInString: d.toLocaleString('default', { month: 'long' }),
-      })
-      d.setMonth(d.getMonth() + 1)
+        mothInString: d.toLocaleString("default", { month: "long" }),
+      });
+      d.setMonth(d.getMonth() + 1);
     }
     let finaldata = result.map((e, i) => {
       return {
@@ -72,63 +74,81 @@ const ChakraCalender = ({
           { length: getDays(e.year, e.mothInNumber) },
           (_, i) => i + 1
         ),
-      }
-    })
-    return finaldata
+      };
+    });
+    return finaldata;
   }
 
-  const day = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+  const day = ["S", "M", "T", "W", "T", "F", "S"];
   return (
     <Flex
       gap={legendGap}
-      flexDir={'column'}
+      flexDir={"column"}
       padding={padding}
       bg={backgroundColor}
       {...others}
     >
-      <Flex flexWrap={'wrap'} gap={gridGap}>
+      <Flex flexWrap={"wrap"} gap={gridGap}>
         {getMonthNames().map((e) => {
           return (
             <Flex
-              height={'auto'}
-              flexWrap='wrap'
-              gap={'4px'}
-              flexDir={'column'}
+              height={"auto"}
+              flexWrap="wrap"
+              flexDir={"column"}
+              gap={"6px"}
             >
               {monthsLabel && (
                 <Text
+                  margin={0}
                   fontSize={monthsLabelFontSize}
-                  fontWeight={'500'}
-                  textAlign='center'
+                  fontWeight={"500"}
+                  textAlign="center"
                 >
                   {`${e.mothInString}, ${e.year}`}
                 </Text>
               )}
               <SimpleGrid columns={7} spacing={cellSpecing}>
                 {e.daysInArray.map((x) => {
-                  const y = `${e.year}-${e.month}-${x}`
-                  const isInclude = Object.keys(values).includes(y)
-                  const colorIndex = values[y]
+                  const y = `${e.year}-${e.month}-${x}`;
+                  const isInclude = Object.keys(values).includes(y);
+                  const colorIndex = values[y]?.value ?? colors[0];
+
+                  const message = values[y]?.message ?? "";
 
                   return (
-                    <Flex flexDir={'column'}>
+                    <Flex flexDir={"column"} gap={"4px"}>
                       {daysLabel && (
                         <>
                           {x <= 7 && (
-                            <Text fontSize={daysLabelFontSize}>
+                            <Text m={0} fontSize={daysLabelFontSize}>
                               {day[x - 1]}
                             </Text>
                           )}
                         </>
                       )}
                       <Tooltip
+                        gap={"0px"}
+                        borderRadius={"4px"}
                         bg={
-                          (isInclude ? colors[colorIndex as any] : colors[0]) ??
-                          colors[0]
+                          !toltip
+                            ? isInclude
+                              ? colors[colorIndex as any]
+                              : colors[0]
+                            : "white"
                         }
                         hasArrow
-                        label={hasTooltip && y}
-                        fontSize='md'
+                        label={
+                          hasTooltip && (
+                            <>
+                              {toltip ? (
+                                toltip(y, message)
+                              ) : (
+                                <Text>{`${y} ${message}`}</Text>
+                              )}
+                            </>
+                          )
+                        }
+                        fontSize="md"
                       >
                         <Box
                           bgColor={
@@ -142,22 +162,22 @@ const ChakraCalender = ({
                         ></Box>
                       </Tooltip>
                     </Flex>
-                  )
+                  );
                 })}
               </SimpleGrid>
             </Flex>
-          )
+          );
         })}
       </Flex>
-      <Flex width={'100%'} justifyContent={'flex-end'} padding={'12px'}>
-        <Flex flexWrap={'wrap'} gap={legendLabelGap} h={'20px'}>
+      <Flex width={"100%"} justifyContent={"flex-end"} padding={"12px"}>
+        <Flex flexWrap={"wrap"} gap={legendLabelGap} h={"20px"}>
           {legend.map((e, i) => (
-            <Flex gap={'4px'} alignItems={'center'}>
+            <Flex gap={"4px"} alignItems={"center"}>
               <Box
                 borderRadius={legendLabelBorderRadius}
                 bg={colors[i + 1] ?? colors[0]}
-                h={'12px'}
-                w={'24px'}
+                h={"12px"}
+                w={"24px"}
               ></Box>
               <Text fontSize={legendLabelFontSize}>{e}</Text>
             </Flex>
@@ -165,6 +185,6 @@ const ChakraCalender = ({
         </Flex>
       </Flex>
     </Flex>
-  )
-}
-export default ChakraCalender
+  );
+};
+export default ChakraCalender;
