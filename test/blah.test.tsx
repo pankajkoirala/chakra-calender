8d668ec1f8cc061de7f76ab39e2246e46f8c2505
pankajkoirala <pankaj.koirala.77@gmail.com>
2022-11-06 18:48:07 +0545

    fix: jsn file and error

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ChakraCalender } from '../src';
+import { Thing } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ChakraCalender />, div);
+    ReactDOM.render(<Thing />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
